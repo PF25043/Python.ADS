136a836429c932d54b2092890752d1ad453cd904
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-20 15:41:04 -0300

    Atividade-Pratica

--- ATIVIDADE-PRATICA2021-ORIENTACOESeEXERCICIOS-MODULO C.py
+++ ATIVIDADE-PRATICA2021-ORIENTACOESeEXERCICIOS-MODULO C.py
@@ -1,27 +1,18 @@
 #nome: Pâmela
 #idade (RU): 28
-nome= input('Nome da criança: ')
-idade= int(input('Idade: '))
-if(idade<=5):
-    print('A aluna {} tem {} anos e está no Ensino Infantil.'.format(nome,idade))
-elif(idade<=10):
-    print('A aluna {} tem {} anos e está no Ensino Fundamental | .'.format(nome, idade))
-elif(idade<=14):
-    print('A aluna {} tem {} anos e está no Ensino Fundamental ||.'.format(nome, idade))
-elif(idade>=15):
-    print('A aluna {} tem {} anos e está no Ensino Médio.'.format(nome, idade))
-cont= int(input('Deseja continuar?   0 - Não  1 - Sim  '))
-
-x = cont
-while(x==1):
+x = 1 #Obrigatório a variavel iniciar com o valor 1 para poder iniciar o programa
+while(x==1): #Estrutura de repetição
     nome = input('Nome da criança: ')
     idade = int(input('Idade: '))
-    if (idade <= 5):
+    if (idade <= 5): #1 a 5 anos
         print('A aluna {} tem {} anos e está no Ensino Infantil.'.format(nome, idade))
-    elif (idade <= 10):
+    elif (idade <= 10): #6 a 10 anos
         print('A aluna {} tem {} anos e está no Ensino Fundamental | .'.format(nome, idade))
-    elif (idade <= 14):
+    elif (idade <= 14): #11 a 14
         print('A aluna {} tem {} anos e está no Ensino Fundamental ||.'.format(nome, idade))
-    elif (idade >= 15):
+    elif (idade >= 15): #maiores de 15 anos
         print('A aluna {} tem {} anos e está no Ensino Médio.'.format(nome, idade))
     cont= int(input('Deseja continuar?   0 - Não  1 - Sim    '))
+    x = cont # Atribuir a responsta do usuario (0 ou 1) à variável X
+    print('------------------------------') #Para separar as consultas
+    print('------------------------------') #Para separar as consultas
