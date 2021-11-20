43afb9c306f5b24e027ea106d89eb39bca36d11d
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-20 14:25:43 -0300

    aula4.ex07

---
+++ Aula04/Ex07.py
@@ -0,0 +1,18 @@
+print('-------------------')
+print('FOGUETE')
+print('-------------------')
+x = 10
+while( x >= 0):
+    print(x)
+    x = x-1
+print('Fogo!!!')
+
+print('-------------------')
+print('-----CONTAGEM----')
+print('-------------------')
+inic = int(input('Qual valor deseja iniciar a contagem? '))
+final = int(input('Qual valor deseja finalizar a contagem? '))
+x = inic
+while( x <= final):
+    print(x)
+    x = x+1
