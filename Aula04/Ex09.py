992748e9cbb0cf66042df49d75790e51c2c30a40
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-20 14:40:48 -0300

    aula4.ex09

---
+++ Aula04/Ex09.py
@@ -0,0 +1,8 @@
+print('-----------------------')
+print('--------TABOADA--------')
+print('-----------------------')
+n1= int(input('Digite um número para saber a taboada dele: '))
+x = 1
+while(x <= 10):
+    print('{} x {} = {}'.format(x,n1,x*n1))
+    x = x+1
