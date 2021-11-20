dadbee6354eea35f3210f5a0c763bd00551705ed
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-20 17:37:55 -0300

    aula4.ex12

---
+++ Aula04/Ex12.py
@@ -0,0 +1,12 @@
+#mostrar os numeros de 10 a 1000
+for x in range(10,1001):
+    print(x)
+
+print('-----------------------------')
+print('-----------------------------')
+print('-----------------------------')
+
+#contagem regressiva 10 até 0 e escrever 'Fogo!' no final da contagem
+for contagem in range(10,-1,-1):
+    print(contagem)
+print('Fogo!')
