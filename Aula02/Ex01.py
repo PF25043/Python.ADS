83cf124670233aff643df515bec80871f123af0e
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-20 12:33:00 -0300

    aula2.ex01

---
+++ Aula02/Ex01.py
@@ -0,0 +1,9 @@
+print('Olá, Mundo!!!')
+print('------------------------------')
+print('--------Exercício01------------')
+print('------------------------------')
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+total = n1+n2
+print('A soma do número {} e {} é {}'.format(n1, n2, total))
