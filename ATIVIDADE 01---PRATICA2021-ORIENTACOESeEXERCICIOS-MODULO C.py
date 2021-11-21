72a32bede17037be48cd5e2fec235ffefa0a9656
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-21 00:39:59 -0300

    aula5.ex16

--- ATIVIDADE 01---PRATICA2021-ORIENTACOESeEXERCICIOS-MODULO C.py
+++ ATIVIDADE 01---PRATICA2021-ORIENTACOESeEXERCICIOS-MODULO C.py
@@ -8,7 +8,7 @@ while(x==1): #Estrutura de repetição
         print('A aluna {} tem {} anos e está no Ensino Infantil.'.format(nome, idade))
     elif (idade <= 10): #6 a 10 anos
         print('A aluna {} tem {} anos e está no Ensino Fundamental | .'.format(nome, idade))
-    elif (idade <= 14): #11 a 14
+    elif (idade <= 14): #11 a 14 anos
         print('A aluna {} tem {} anos e está no Ensino Fundamental ||.'.format(nome, idade))
     elif (idade >= 15): #maiores de 15 anos
         print('A aluna {} tem {} anos e está no Ensino Médio.'.format(nome, idade))
---
+++ Aula5/Ex16.py
@@ -0,0 +1,12 @@
+# FUNÇÕES
+
+# def nomeDaFunção ( ) :
+
+#FUNÇÃO
+def linhas ():
+    print('|','___'*10,'|') #para diminuir o codigo multipliquei os traços por 10.
+    print('|','___'*10,'|')
+#PROGRAMA PRINCIPAL
+linhas()
+print('               MENU')
+linhas()
