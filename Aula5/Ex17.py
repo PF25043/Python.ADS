05ba52e610691c65e014b297e6b53378639878c3
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-21 01:17:00 -0300

    aula5.ex17 e ex18

---
+++ Aula5/Ex17.py
@@ -0,0 +1,13 @@
+# FUNÇÕES COM PARÂMETROS
+
+# def nomeDaFunção (variável) :
+
+def linhas (s1): #inserir a variavel no parenteses
+    print('|','___'*10,'|') #para diminuir o codigo multipliquei os traços por 10.
+    print('|','___'*10,'|')
+    print(s1)     # onde a variavel irá aparecer no texto
+    print('|', '___' * 10, '|')
+    print('|', '___' * 10,'|')
+
+linhas('             MENU') #texto que deverá aparecer na variável s1 descrita na função acima
+
---
+++ Aula5/Ex18.py
@@ -0,0 +1,14 @@
+#função com mais de um parâmetro
+
+#Função
+def sub2(x,y):
+    res = x-y
+    print(res)
+
+#Programa principal
+sub2(10,5) #função de subtração
+sub2(5,7)
+sub2(1677,23)
+
+
+
