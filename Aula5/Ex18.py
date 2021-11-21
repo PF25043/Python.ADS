14636d837489859288146867b4216f3419b209b9
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-11-21 01:26:57 -0300

    aula5.ex17 e ex18/2

--- Aula5/Ex18.py
+++ Aula5/Ex18.py
@@ -10,5 +10,18 @@ sub2(10,5) #função de subtração
 sub2(5,7)
 sub2(1677,23)
 
+print('|','___'*10,'|')
+print('|','___'*10,'|')
+
+#Função com parâmetro opcional
+def soma(x=0,y=0,z=0): #atribuimos um valor à variàvel e assim caso ela fique de fora a função irá funcionar normalmente
+    res = x+y+z
+    print(res)
+
+#Programa principal
+soma(10,5) # só é possivel somar 2 nº ao inves de 3 pq atribuimos o valor zero às variàveis
+soma(5,7,10)
+
+
 
 
